ec43be52416e12478a9390546ce4a56b335b592b
Echo-m1 <dubravin.mo@mail.ru>
2026-02-24 01:41:41 +0300

    people: оптимизация генерации данных

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from '@components/Layout/Layout'
 import Dashboard from '@pages/Dashboard/Dashboard'
 import PeopleList from '@pages/PeopleList/PeopleList'
 import PersonCard from '@pages/PersonCard/PersonCard'
+import { setPeople } from '@store/slices/peopleSlice'
+import { generatePeople } from '@data/generatePeople'
 
 function App() {
+  const dispatch = useDispatch()
+  const peopleCount = useSelector((state) => state.people.items.length)
+
+  useEffect(() => {
+    if (peopleCount === 0) {
+      dispatch(setPeople(generatePeople(25000, { min: 20000, max: 30000 })))
+    }
+  }, [peopleCount, dispatch])
+
   return (
     <BrowserRouter>
       <Layout>
